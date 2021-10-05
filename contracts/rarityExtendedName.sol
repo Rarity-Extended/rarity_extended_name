ffdc9ec325ae79a81b6a13e540ad3af6b674ffa4
Major-Eth <major-tom.eth@pm.me>
2021-10-05 23:10:43 +0200

    ✨ Simplification

--- contracts/rarityExtendedName.sol
+++ contracts/rarityExtendedName.sol
@@ -10,7 +10,7 @@ interface IRarity {
 contract rarity_extended_name {
     IRarity constant _rm = IRarity(0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb);
     string constant public name = "Rarity Extended Name";
-    mapping(uint => string[3]) private adventurers_name;
+    mapping(uint => string) private adventurers_name;
 
     /**
     **  @dev Check if the msg.sender has the autorization to act on this adventurer
@@ -23,50 +23,18 @@ contract rarity_extended_name {
     /**
     **  @dev set the name of an adventurer
     **  @param _adventurer tokenID of the adventurer to assign a name to
-    **  @param _firstname firstname to assign to this adventurer
-    **  @param _lastname lastname to assign to this adventurer
-    **  @param _surname surname to assign to this adventurer
+    **  @param _name name to assign to this adventurer
     */
-    function set_name(uint _adventurer, string memory _firstname, string memory _lastname, string memory _surname) external {
+    function set_name(uint _adventurer, string memory _name) external {
         require(_isApprovedOrOwner(_adventurer));
-        adventurers_name[_adventurer] = [_firstname, _lastname, _surname];
-    }
-
-    /**
-    **  @dev set the firstname of an adventurer
-    **  @param _adventurer tokenID of the adventurer to assign a firstname to
-    **  @param _firstname firstname to assign to this adventurer
-    */
-    function set_firstname(uint _adventurer, string memory _firstname) external {
-        require(_isApprovedOrOwner(_adventurer));
-        adventurers_name[_adventurer][0] = _firstname;
-    }
-
-    /**
-    **  @dev set the lastname of an adventurer
-    **  @param _adventurer tokenID of the adventurer to assign a lastname to
-    **  @param _lastname lastname to assign to this adventurer
-    */
-    function set_lastname(uint _adventurer, string memory _lastname) external {
-        require(_isApprovedOrOwner(_adventurer));
-        adventurers_name[_adventurer][1] = _lastname;
-    }
-
-    /**
-    **  @dev set the surname of an adventurer
-    **  @param _adventurer tokenID of the adventurer to assign a surname to
-    **  @param _surname surname to assign to this adventurer
-    */
-    function set_surname(uint _adventurer, string memory _surname) external {
-        require(_isApprovedOrOwner(_adventurer));
-        adventurers_name[_adventurer][2] = _surname;
+        adventurers_name[_adventurer] = _name;
     }
     
     /**
     **  @dev get the different parts of the name of the adventurer
     **  @param _adventurer tokenID of the adventurer to get name of
     */
-    function get_name(uint _adventurer) public view returns (string memory, string memory, string memory) {
-        return (adventurers_name[_adventurer][0], adventurers_name[_adventurer][1], adventurers_name[_adventurer][2]);
+    function get_name(uint _adventurer) public view returns (string memory) {
+        return (adventurers_name[_adventurer]);
     }
 }
--- test/tests.js
+++ test/tests.js
@@ -31,49 +31,21 @@ describe('Tests', () => {
 		await	expect(name).to.be.equal('Rarity Extended Name');
 	})
 
-	it('should be possible to summon 4 adventurers', async function() {
-		for (let index = 1; index < 5; index++) {
-			const	nextAdventurer = Number(await RARITY.next_summoner());
-			await	(await RARITY.summon(index)).wait();
-			adventurerPool.push(nextAdventurer);	
-		}
+	it('should be possible to summon 1 adventurer', async function() {
+		const	nextAdventurer = Number(await RARITY.next_summoner());
+		await	(await RARITY.summon(1)).wait();
+		adventurerPool.push(nextAdventurer);	
 	})
 
 	it('should be possible to set_name', async function() {
-		await expect(rarityExtendedName.set_name(adventurerPool[0], 'John', 'Doe', 'The Unknown', {from: user.address})).not.to.be.reverted;
-	})
-	it('should be possible to set_firstname', async function() {
-		await expect(rarityExtendedName.set_firstname(adventurerPool[1], 'Jane', {from: user.address})).not.to.be.reverted;
-	})
-	it('should be possible to set_lastname', async function() {
-		await expect(rarityExtendedName.set_lastname(adventurerPool[2], 'Deaa', {from: user.address})).not.to.be.reverted;
-	})
-	it('should be possible to set_surname', async function() {
-		await expect(rarityExtendedName.set_surname(adventurerPool[3], 'Unname', {from: user.address})).not.to.be.reverted;
+		await expect(rarityExtendedName.set_name(adventurerPool[0], 'John', {from: user.address})).not.to.be.reverted;
 	})
 	
-	it('should be possible to get the name of Adventurer 1', async function() {
+	it('should be possible to get the name of the Adventurer', async function() {
 		const	names = await rarityExtendedName.get_name(adventurerPool[0]);
-		expect(names[0]).to.be.equal('John');
-		expect(names[1]).to.be.equal('Doe');
-		expect(names[2]).to.be.equal('The Unknown');
+		expect(names).to.be.equal('John');
 	})
-	it('should be possible to get the name of Adventurer 2', async function() {
-		const	names = await rarityExtendedName.get_name(adventurerPool[1]);
-		expect(names[0]).to.be.equal('Jane');
-		expect(names[1]).to.be.equal('');
-		expect(names[2]).to.be.equal('');
-	})
-	it('should be possible to get the name of Adventurer 3', async function() {
-		const	names = await rarityExtendedName.get_name(adventurerPool[2]);
-		expect(names[0]).to.be.equal('');
-		expect(names[1]).to.be.equal('Deaa');
-		expect(names[2]).to.be.equal('');
-	})
-	it('should be possible to get the name of Adventurer 4', async function() {
-		const	names = await rarityExtendedName.get_name(adventurerPool[3]);
-		expect(names[0]).to.be.equal('');
-		expect(names[1]).to.be.equal('');
-		expect(names[2]).to.be.equal('Unname');
+	it('should not be possible for another user to update the name of non-owned adventurer', async function() {
+		await expect(rarityExtendedName.set_name(adventurerPool[0], 'Jane', {from: anotherUser.address})).to.be.reverted;
 	})
 });
